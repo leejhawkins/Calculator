0bc2433102df65ca7a268b7c83ebb4b2b70fd7f4
leejhawkins <59981819+leejhawkins@users.noreply.github.com>
2021-02-21 20:17:42 -0700

    Initial commit

---
+++ force-app/main/default/lwc/simpleCalculator/simpleCalculator.js
@@ -0,0 +1,27 @@
+import { LightningElement, track } from 'lwc';
+
+export default class SimpleCalculator extends LightningElement {
+    currentResult = "Result goes here";
+    firstNumber;
+    secondNumber;
+    numberChangeHandler(event) {
+        const field = event.target.name
+        if (field === 'firstNumber'){
+            this.firstNumber = event.target.value;
+        } else {
+            this.secondNumber = event.target.value;
+        }
+    }
+    addHandler(){
+        this.currentResult = `The result of ${parseInt(this.firstNumber)} + ${parseInt(this.secondNumber)} is ${parseInt(this.firstNumber) + parseInt(this.secondNumber)} `
+    }
+    subtractHandler(){
+        this.currentResult = `The result of ${parseInt(this.firstNumber)} - ${parseInt(this.secondNumber)} is ${parseInt(this.firstNumber) - parseInt(this.secondNumber)} `
+    }
+    multiplyHandler(){
+        this.currentResult = `The result of ${parseInt(this.firstNumber)} x ${parseInt(this.secondNumber)} is ${parseInt(this.firstNumber) * parseInt(this.secondNumber)} `
+    }
+    divideHandler(){
+        this.currentResult = `The result of ${parseInt(this.firstNumber)} ÷ ${parseInt(this.secondNumber)} is ${parseInt(this.firstNumber) /  parseInt(this.secondNumber)} `
+    }
+}
